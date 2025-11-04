9093f2cfca095edbd29a8508feebc3d373209310
Ilam Ball <informasiilam@gmail.com>
2025-11-04 09:25:36 +0700

    Add Film model definition for Sequelize

---
+++ models/film.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+    const Film = sequelize.define('Film', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nama_film: {
+            type: DataTypes.STRING,
+        },
+        deskripsi: {
+            type: DataTypes.STRING,
+        },
+        sutradara: {
+            type: DataTypes.STRING,
+        }
+    });
+    return Film;
+}
